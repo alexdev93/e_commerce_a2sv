343964b3d955ec986445fae8639fe184a298986d
alexdev93 <alemayehu.dev@gmail.com>
2025-11-12 00:09:07 +0300

    feat(backend): add docker compose file

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -10,7 +10,7 @@ const {
   DB_PORT,
   DB_USERNAME,
   DB_PASSWORD,
-  DB_NAME,
+  DB_DATABASE,
   DB_SYNC
 } = process.env;
 
@@ -21,7 +21,7 @@ export const AppDataSource = new DataSource({
   port: Number(DB_PORT),
   username: DB_USERNAME,
   password: DB_PASSWORD,
-  database: DB_NAME,
+  database: DB_DATABASE,
   synchronize: true,
   logging: false,
   entities: [User, Product, Order],
